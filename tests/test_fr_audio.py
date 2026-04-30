533e63d5ccf5a42136316a1acf768948648ed1d4
Yenyen Wang <wangyenyen.st00g@g2.nctu.edu.tw>
2026-04-29 23:03:00 -0400

    update unit test file

--- tests/test_fr_audio.py
+++ tests/test_fr_audio.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import pytest
 
+
 def test_fetch_verb_conjugation():
     expected_result = "j'ai\ttu as\til a\telle a\ton a\tnous avons\tvous avez\tils ont\telles ont".split(
         "\t"
